6ae0f0ef3fe66d31897cb27e526a6d454b6bb193
Heis-mariyo <yoyoizunobi@gmail.com>
2026-04-20 12:14:43 +0100

    theme colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFF97316);
+  static const background = Color(0xFF0B0F1A);
+  static const surface = Color(0xFF111827);
+  static const surfaceLight = Color(0xFF1F2937);
+  static const textPrimary = Color(0xFFFFFFFF);
+  static const textSecondary = Color(0xFF9CA3AF);
+  static const textMuted = Color(0xFF6B7280);
+  static const success = Color(0xFF22C55E);
+  static const error = Color(0xFFEF4444);
+  static const border = Color(0xFF374151);
+}
